faad41f6e91bd8d358e067f4db41bb0faa4d0bfc
Remar59 <remlevy@hotmail.fr>
2023-12-29 13:55:41 +0100

    Adding characters meter + delete all button

--- src/App.js
+++ src/App.js
@@ -16,47 +16,124 @@ export default class App extends Component {
     });
   }
 
-  addTask() {
-    const newTask = {
-      id: Date.now(),
-      name: this.state.inputName
-    }
-    const copyTasks = [...this.state.tasks];
-    copyTasks.push(newTask);
+  //Ajout d'une tâche (avec nbre de carac min de 5, max de 75)
 
-    this.setState({
-      tasks: copyTasks,
-      inputName: ""
-    })
+  addTask() {
+    const minlength = 5;
+    const maxlength = 75;
+    const { inputName } = this.state;
+
+    if (this.state.inputName.length >= minlength && this.state.inputName.length <= maxlength) {
+      const newTask = {
+        id: Date.now(),
+        name: this.state.inputName
+      };
+
+      const copyTasks = [...this.state.tasks];
+      copyTasks.push(newTask);
+
+      this.setState({
+        tasks: copyTasks,
+        inputName: ""
+      });
+    } else if (inputName.length < minlength) {
+      alert(`Merci d'entrer au minimum 5 caractères.`);
+    } else {
+      alert(`Vous ne pouvez pas entrer plus de 75 caractère.`);
+    }
   }
 
+  // suppression d'une tâche (nécessite un état checked, voir plus bas checkbox)
   deleteTask(index) {
-    const selectedTask = { ...this.state.tasks};
-    selectedTask.splice(index,1)
+    const copyTasks = [...this.state.tasks];
+    if (copyTasks[index].checked) {
+      copyTasks.splice(index, 1);
+      this.setState({
+        tasks: copyTasks
+      });
+    } else {
+      alert("Veuillez cocher la case devant la tâche afin de valider la suppression.")
+    }
+  }
+
+  // tout supprimer 
+  deleteAll() {
+    const confirmDelete = window.confirm("Êtes-vous sûr de vouloir supprimer toutes les tâches?");
+    if (confirmDelete) {
+      this.setState({
+        tasks: []
+      });
+    }
+  }
+
+// checkbox à valider afin de supprimer individuellement les tâches
+  checkBoxDelete(index) {
+    const copyTasks = [...this.state.tasks];
+    copyTasks[index].checked = !copyTasks[index].checked;
 
     this.setState({
-      selectedTask
-  });
+      tasks: copyTasks
+    });
+  }
 
+  // Permet d'indiquer la limite du nbre de caractères
+  charactersCounter() {
+    const MeterValue = document.getElementById("myMeter");
+    const myInput = document.getElementById("mainInput");
+    const counterPlace = document.getElementById("counterPlace");
+    const maxCharacters = 75;
+
+    let remainingCharacters = maxCharacters - myInput.value.length;
+
+    remainingCharacters = Math.max(0, remainingCharacters);
+
+    MeterValue.setAttribute("value", myInput.value.length);
+    counterPlace.innerHTML = `Limite de caractères: ${remainingCharacters}`;
   }
 
   render() {
-    const listTasks = this.state.tasks.map((task) => {
+    const listTasks = this.state.tasks.map((task, index) => {
       return (
-        <Task name={task.name} key={task.id} />
-      )
+        <Task
+          name={task.name}
+          key={task.id}
+          checked={task.checked}
+          action={() => this.checkBoxDelete(index)}
+          deleteAction={() => this.deleteTask(index)}
+        />
+      );
     });
 
+
     return (
-      <div className="container">
-        <div className="main"><h1>Taskinator</h1>
-          <input type="text" value={this.state.inputName} onChange={(e) => this.changeInputName(e.target.value)} />
-          <button onClick={() => this.addTask()}>Ajouter</button>
-          <h2>Mes tâches</h2>
-          <ul>
-            {listTasks}<input type="button" value="Supprimer" />
-          </ul>
-        </div></div>
+      <div class="container">
+        <div class="title"><img id="brain" src="/brain.png" alt="brain" /><h1>Taskinator</h1><img id="robot" src="/robot.png" alt="brain" /><div class="hr"></div></div>
+        <div class="form">
+          <div id="counter"><meter id="myMeter" min="0" max="75" low="25" high="66" optimum="80"></meter>
+            <h3 id="counterPlace"></h3></div>
+          <input
+            id="mainInput"
+            type="text"
+            value={this.state.inputName}
+            onChange={(e) => {
+              this.changeInputName(e.target.value);
+              this.charactersCounter();
+            }}
+            //Permet d'entrer la tâche en appuyant sur entrée
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                this.addTask();
+              }
+            }}
+          />
+          <button id="add" onClick={() => this.addTask()}>Ajouter</button>
+          <button id="deleteAll" onClick={() => this.deleteAll()}>Supprimer toutes les tâches</button></div>
+        <div class="render">
+          <h2>Mes tâches</h2></div>
+        <ul class="list">
+          {listTasks}
+        </ul>
+      </div>
     )
 
 
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import "../App.scss";
 
 
-const Task = ({ name, action }) => {
+const Task = ({ name, checked, action, deleteAction }) => {
+    const taskChecked = checked ? 'strike' : '';
+
     return (
-        <li><input type="checkbox" /><input type="text" value={name} onChange={action} /><input type="button" value="Supprimer" onClick={action} /></li>
-    )
-}
+        <li>
+            <input type="checkbox" checked={checked} onChange={action} />
+            <p minlength='4' className={taskChecked}>{name}</p>
+            <input id='delete' type="button" value="Supprimer la tâche" onClick={deleteAction} />
+        </li>
+    );
+};
 
 export default Task
